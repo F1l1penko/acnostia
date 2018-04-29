875a377ad69ca62e9c53eb31d2a887fefd67fb16
unknown <you@example.com>
2018-04-29 22:45:01 +0300

    fourth

--- web/js/main.js
+++ web/js/main.js
@@ -1,2 +1,2 @@
-"use strict";$(document).ready(function(){$("#fullpage").fullpage({anchors:["firstPage","secondPage","thirdPage","fourthPage","fivePage"],menu:"#menu",slidesNavigation:!0})});
+"use strict";$(document).ready(function(){$("#fullpage").fullpage({anchors:["firstPage","secondPage","thirdPage","fourthPage","fivePage"],menu:"#menu",slidesNavigation:!0}),$("portfolio-item").isotope({itemSelector:".item",layoutMode:"fitRows"}),$(".portfolio-menu ul li").click(function(){$(".portfolio-menu ul li").removeClass("active"),$(this).addClass("active");var e=$(this).attr("data-filter");return $(".portfolio-item").isotope({filter:e}),!1})});
 //# sourceMappingURL=main.js.map
